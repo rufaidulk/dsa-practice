1e8edb88824e9dc84c79b7487e04d89a515a6262
rufaidulk <35684474+rufaidulk@users.noreply.github.com>
2023-07-27 08:50:23 +0530

    Update SetMatrixZeroesBrute.java

--- Medium/SetMatrixZeroesBrute.java
+++ Medium/SetMatrixZeroesBrute.java
@@ -5,7 +5,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {    
                 if (matrix[i][j] == 0) {
                     this.markAsNegative(matrix, i, j);
                 }
@@ -22,23 +22,16 @@ class Solution {
     }
 
     private void markAsNegative(int[][] matrix, int row, int col) {
-        int i = row;
-        while (++i < matrix.length && matrix[i][col] != 0) {
-            matrix[i][col] = -1;
-        }
-        i = row;
-        while (--i >= 0 && matrix[i][col] != 0) {
-            matrix[i][col] = -1;
+        for (int i = 0; i < matrix[row].length; i++) {
+            if (matrix[row][i] != 0) {
+                matrix[row][i] = -1;
+            }
         }
 
-        i = col;
-        while (--i >= 0 && matrix[row][i] != 0) {
-            matrix[row][i] = -1;
-        }
-
-        i = col;
-        while (++i < matrix[row].length && matrix[row][i] != 0) {
-            matrix[row][i] = -1;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][col] != 0) {
+                matrix[i][col] = -1;
+            }
         }
     }
 }
